983a490c885a7655b812f507de65d6c93fbabee4
blue <blue.yuan@orderplus.com>
2021-04-29 11:08:46 +0800

    push code

---
+++ docs/.vuepress/components/demo.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+    <div>123</div>
+    <div>{{msg}}</div>
+    <div>
+        <img :src="$withBase('/images/picker-logo.png')" alt="">
+    </div>
+   </div>
+</template>
+<script>
+export default {
+    name:'demo',
+    data () {
+        return {
+            msg:'123'
+        }
+    }
+}
+</script>

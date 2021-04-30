127d0df87269def987a755b863abb85684edbddd
blue <blue.yuan@orderplus.com>
2021-04-30 16:06:14 +0800

    提交代码

--- docs/.vuepress/components/demo.vue
+++ docs/.vuepress/components/demo.vue
@@ -1,19 +1,68 @@
 <template>
-   <div>
-    <div>123</div>
-    <div>{{msg}}</div>
-    <div>
-        <img :src="$withBase('/images/picker-logo.png')" alt="">
-    </div>
-   </div>
+	<div>
+		<div class="panels" ref="box">
+			<div class="panel panel1">
+				<p>Hey</p>
+				<p>Let's</p>
+				<p>Dance</p>
+			</div>
+			<div class="panel panel2">
+				<p>Give</p>
+				<p>Take</p>
+				<p>Receive</p>
+			</div>
+			<div class="panel panel3">
+				<p>Experience</p>
+				<p>It</p>
+				<p>Today</p>
+			</div>
+			<div class="panel panel4">
+				<p>Give</p>
+				<p>All</p>
+				<p>You can</p>
+			</div>
+			<div class="panel panel5">
+				<p>Life</p>
+				<p>In</p>
+				<p>Motion</p>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-    name:'demo',
-    data () {
-        return {
-            msg:'123'
-        }
-    }
-}
+	name: 'demo',
+	data() {
+		return {
+			theme: '',
+		};
+	},
+	methods: {
+		change(e) {
+			const panels = document.getElementsByClassName('panel');
+
+			panels.forEach((panel) =>
+				panel.addEventListener('click', toggleOpen)
+			);
+			panels.forEach((panel) =>
+				panel.addEventListener('transitionend', toggleActive)
+			);
+
+			function toggleOpen() {
+				this.classList.toggle('open');
+			}
+
+			function toggleActive(e) {
+				// 因为浏览器的差异，有的浏览器显示 flex，有的浏览器显示 flex-grow，因此使用 includes()
+				if (e.propertyName.includes('flex')) {
+					this.classList.toggle('open-active');
+				}
+			}
+		},
+	},
+	mounted() {
+		this.change();
+	},
+};
+</script>
 </script>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
+  title: "MASK",
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: '/images/mask.png' }]
+  ],
+  themeConfig: {
+    logo: '/images/picker-logo.png',
+    nav:[
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      }
+    ]
   }
+}
